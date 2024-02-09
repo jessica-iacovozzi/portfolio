8d00fa70d2b2b505ef2bba6aff54123589d74c4f
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2024-02-08 22:56:11 -0500

    Added issue tracker project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,7 +8,7 @@ export default function Project({ project }) {
 
   return (
     <div className='inline lg:flex items-center lg:h-5/6 pb-10'>
-      <div className="lg:w-1/2 flex-1 flex flex-col items-center lg:items-start p-10 lg:p-28">
+      <div className="lg:w-1/2 flex-1 flex flex-col items-center lg:items-start px-10 py-5 lg:p-28">
         <div className="flex flex-col justify-evenly text-center lg:text-start">
           <h3 className="text-5xl text-white font-bold">{project.name}</h3>
           <p className="text-gray-400 text-xl font-semibold mt-8 lg:me-8" dangerouslySetInnerHTML={{ __html: project.description }}></p>
@@ -21,7 +21,7 @@ export default function Project({ project }) {
           </a> */}
         </div>
       </div>
-      <div className='flex-1 flex lg:h-3/4 lg:me-20'>
+      <div className='flex-1 flex lg:h-3/4 lg:me-20 pt-20'>
         <img src={project.image} alt="mockup" className='project-img' />
       </div>
     </div>
--- src/projects_data.js
+++ src/projects_data.js
@@ -2,6 +2,7 @@ import CharityFinder from './assets/charity_finder.png';
 import Rmr from './assets/rmr.png';
 import DogBreedHub from './assets/db_hub.png';
 import ShortURL from './assets/shorturl.png';
+import IssueTracker from './assets/issuetracker.png';
 import i18n from './i18n';
 
 const projects = [
@@ -24,6 +25,12 @@ const projects = [
     image: DogBreedHub
   },
   {
+    name: "Issue Tracker",
+    appLink: "https://issue-tracker-alpha-eight.vercel.app/",
+    codeLink: "https://github.com/jessica-iacovozzi/issue-tracker",
+    image: IssueTracker
+  },
+  {
     name: "ShortURL",
     appLink: "https://shorturl.ltd/",
     codeLink: "https://github.com/jessica-iacovozzi/url_shortener",
@@ -35,7 +42,8 @@ function fill() {
   projects[0].description = i18n.t('rmr_description'),
   projects[1].description = i18n.t('cf_description'),
   projects[2].description = i18n.t('dbh_description'),
-  projects[3].description = i18n.t('surl_description')
+  projects[3].description = i18n.t('it_description'),
+  projects[4].description = i18n.t('surl_description')
 }
 
 fill();
